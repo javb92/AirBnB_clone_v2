f4338318646f76bf019fc94be71926971fb286cf
javb92 <mentor37_@hotmail.com>
2019-08-13 02:39:26 +0000

    [FIRST test]

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- 4. MySQL setup test
+-- testing
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
