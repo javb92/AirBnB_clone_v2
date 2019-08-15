7031b2be729439b509655be636aa41feb471a996
GioRubioHolberton <759@holbertonschool.com>
2019-08-15 02:21:56 +0000

    correction model city

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -5,5 +5,5 @@ if os.environ.get('HBNB_TYPE_STORAGE') == os.environ.get('HBNB_MYSQL_DB=hbnb_dev
     storage = storage.reload()
 else:
     from models.engine.file_storage import FileStorage
-    storage = FileStorage
+    storage = FileStorage()
     storage = storage.reload()
