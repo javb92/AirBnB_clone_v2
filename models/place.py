4d7ec1b74c9b0409f67120cc8ade416c9eeb6d9a
javb92 <mentor37_@hotmail.com>
2019-08-15 16:46:50 +0000

    [FIX] added more requirements

--- models/place.py
+++ models/place.py
@@ -25,12 +25,12 @@ class Place(BaseModel, Base):
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False)
-    number_bathrooms = Column(Integer, nullable=False)
-    max_guest = Column(Integer, nullable=False)
-    price_by_night = Column(Integer, nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=True)
     reviews = relationship("Review",
                            cascade="all, delete-orphan", backref="place")
 
