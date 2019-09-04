809a660a7e05c0d9756887e36e638b2b293e1826
javb92 <mentor37_@hotmail.com>
2019-09-04 00:18:34 +0000

    [FIRST]

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# task 0
+"""module web_flask
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello2():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
